43200242ffcd631263b0864a0926ab4bad23abec
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-10-10 10:57:12 +0100

    Began work trying to include the json file to html

--- src/convert.js
+++ src/convert.js
@@ -17,11 +17,11 @@ function convertToJSON(data) {
 }
 
 function loadFiles() {
-    var fs = require('fs');
-    var movieData = fs.readFileSync('../data/movie_metadata_subset.csv', { encoding: 'utf8', flag: 'r'});
+    var fs = require("fs");
+    var movieData = fs.readFileSync("../data/movie_metadata_subset.csv", { encoding: "utf8", flag: "r"});
 
     var jsonMovieData = convertToJSON(movieData);
-    fs.writeFileSync('../data/movie_metadata_subset.json', JSON.stringify(jsonMovieData));
+    fs.writeFileSync("../data/movie_metadata_subset.json", JSON.stringify(jsonMovieData));
 }
 
 loadFiles()
