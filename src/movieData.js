526e0e8fb7f281e35d82b8e03ccfaadb7ce75bc4
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-10-11 10:59:27 +0100

    Updated html tformat movie database look right and added clickable items to list

--- src/movieData.js
+++ src/movieData.js
@@ -2,10 +2,17 @@ const url = "../data/movie_metadata_subset.json";
 const movieDataBase = document.querySelector(".movieDataBase");
 const divider = document.createElement("div");
 const ul = document.createElement("ul");
+const movieList = document.getElementById("ul");
 movieDataBase.append(divider);
 movieDataBase.append(ul);
 
-window.addEventListener("DOMContentLoaded", () => {
+const movieQueue = document.querySelector(".movieQueue");
+const queueDiv = document.createElement("div");
+const ol = document.createElement("ol");
+movieQueue.append(queueDiv);
+movieQueue.append(ol);
+
+window.addEventListener("load", () => {
     loadData();
 });
 
@@ -21,6 +28,26 @@ function addToPage(data) {
         const mins = (parseInt(movie.duration) % 60);
         const hours = ((parseInt(movie.duration) - (parseInt(movie.duration) % 60)) / 60);
         li.textContent = movie.movie_title + "(" + movie.director_name + ", " + movie.title_year + ", " + hours + ":" + mins + ")";
+        if (movie.status) {
+            li.classList.add("active");
+        } else {
+            li.classList.add("inactive");
+        }
         ul.append(li);
+        li.addEventListener("click", (e) => {
+            li.classList.toggle("active");
+            li.classList.toggle("inactive");
+        });
     });
 };
+
+function addToQueue() {
+    const item = ul.getElementsByTagName("li");
+    for (let i = 0; i < item.length; i++) {
+        const li = document.createElement("li");
+        if (item[i].classList.st) {
+            console.log(ul[i]);
+            li.textContent = ul[i];
+        }
+    }
+}
