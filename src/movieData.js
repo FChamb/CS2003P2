4bf6f7aab24cae8660c8fb2e5c485b2e149d78dd
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-10-21 15:23:19 +0200

    Added more to implementation. Adding movies to queue, removing. Also added search bar

--- src/movieData.js
+++ src/movieData.js
@@ -1,7 +1,7 @@
 const url = "../data/movie_metadata_subset.json";
 const movieDataBase = document.querySelector(".movieDataBase");
 const divider = document.createElement("div");
-const ul = document.createElement("ul");
+const ul = document.getElementById("database");
 const movieList = document.getElementById("ul");
 movieDataBase.append(divider);
 movieDataBase.append(ul);
@@ -23,32 +23,66 @@ function loadData() {
 };
 
 function addToPage(data) {
+//    ul.addEventListener("click", (e) => {
+//        let li = ol.getElementsByTagName("li");
+//        let valid = false;
+//        for (let i = 0; i < li.length; i++) {
+//            if (li[i].textContent == e.target.textContent) {
+//                valid = true;
+//            }
+//        }
+//        if (!valid) {
+//            ol.appendChild(e.target.cloneNode(true));
+//        } else {
+//            e.target.remove();
+//        }
+//    });
     data.forEach((movie) => {
         const li = document.createElement("li");
         const li2 = document.createElement("li");
         const mins = (parseInt(movie.duration) % 60);
         const hours = ((parseInt(movie.duration) - (parseInt(movie.duration) % 60)) / 60);
         li.textContent = movie.movie_title + "(" + movie.director_name + ", " + movie.title_year + ", " + hours + ":" + mins + ")";
-        if (movie.status) {
+        li2.textContent = li.textContent;
+        if (li.status) {
             li.classList.add("active");
         } else {
             li.classList.add("inactive");
         }
         ul.append(li);
-        li.addEventListener("click", (e) => {
-            li.classList.toggle("active");
-            li.classList.toggle("inactive");
-        });
+        addMovie(li, li2);
     });
 };
 
-// function addToQueue() {
-//     const item = ul.getElementsByTagName("li");
-//     for (let i = 0; i < item.length; i++) {
-//         const li = document.createElement("li");
-//         if (item[i].classList.st) {
-//             console.log(ul[i]);
-//             li.textContent = ul[i];
-//         }
-//     }
-// }
+function addMovie(li, li2) {
+    li.addEventListener("click", (e) => {
+        li.classList.toggle("active");
+        li.classList.toggle("inactive");
+        let active = li.classList;
+        if (active.contains("inactive")) {
+            ol.removeChild(li2);
+        } else {
+            ol.append(li2);
+        }
+//    li2.addEventListener("click", (e) => {
+//        li.classList.toggle("inactive");
+//        li.classList.toggle("active");
+//        ol.removeChild(li2);
+//        });
+    });
+}
+
+function search() {
+    let input = document.getElementById("searchbar");
+    let search = input.value.toLowerCase();
+    let li = ul.getElementsByTagName("li");
+    for (let i = 0; i < li.length; i++) {
+        let text = li[i].textContent.toLowerCase().substring(0, li[i].textContent.indexOf("(") - 1);
+        if (search && text.includes(search)) {
+            li[i].scrollIntoView();
+            li[i].style.backgroundColor = "yellow";
+        } else {
+            li[i].style.backgroundColor = "transparent";
+        }
+    }
+}
