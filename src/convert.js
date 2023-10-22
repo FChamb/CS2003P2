6c2653344f64b792422ba77221f54287687fa705
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-10-22 16:49:37 +0100

    Final code ideally with comments

--- src/convert.js
+++ src/convert.js
@@ -1,3 +1,11 @@
+// convertToJSON takes a provided csv data and returns the proper format in
+// JSON. Different variables for the line splits and category splits are created.
+// A new array with the jsonFormat is created. Using a for loop to cycle through
+// ever line after the first, the data is split at each comma. Another for loop takes each
+// element that corresponds to the category list sets a specific item to its category.
+// Finally the array for jsonFormat is pushed the movieInfo. At the end of this cycle,
+// this array is returned.
+"use strict";
 function convertToJSON(data) {
     var lines = data.split("\n");
     var categories = lines[0].split(",");
@@ -16,6 +24,12 @@ function convertToJSON(data) {
     return jsonFormat;
 }
 
+// loadFiles takes two inputs. A csv input file and a json output file.
+// Using a readFileSync, the csv data is passed to the movieData variable.
+// Next this data is passed to a convertToJSON function and handed to jsonMovieData.
+// Lastly a writeFileSync is passed the jsonMovieData. This writes all of the information
+// into the provided output file.
+"use strict";
 function loadFiles(csv, json) {
     var fs = require("fs");
     var movieData = fs.readFileSync(csv, {encoding: "utf8", flag: "r"});
@@ -24,4 +38,6 @@ function loadFiles(csv, json) {
     fs.writeFileSync(json, JSON.stringify(jsonMovieData));
 }
 
+// Simple call to loadFiles with the appropriate links to csv and json files.
+// Enables this program to run when called from the terminal.
 loadFiles("../data/movie_metadata_subset.csv", "../data/movie_metadata_subset.json")
--- src/movieData.js
+++ src/movieData.js
@@ -1,51 +1,52 @@
-const url = "../data/movie_metadata_subset.json";
+// This body of code sets all of the variables that will be used throughout this
+// program to access various elements of the HTML.
+"use strict";
+const json = "../data/movie_metadata_subset.json";
 const movieDataBase = document.querySelector(".movieDataBase");
-const divider = document.createElement("div");
 const ul = document.getElementById("database");
-const movieList = document.getElementById("ul");
-movieDataBase.append(divider);
-movieDataBase.append(ul);
-
 const movieQueue = document.querySelector(".movieQueue");
-const queueDiv = document.createElement("div");
-const ol = document.createElement("ol");
-movieQueue.append(queueDiv);
-movieQueue.append(ol);
+const ol = document.getElementById("queue");
+var totalTime = 0;
 
+// This function begins the process of loading the data from the json file into the appropriate
+// list once the webpage has loaded.
+"use strict";
 window.addEventListener("load", () => {
     loadData();
 });
 
+// Load data grabs the json file with all of the movie data and passes it into a json
+// parser which then hands the data to the next method.
+"use strict";
 function loadData() {
-    fetch(url).then(rep => rep.json()).then((data) => {
+    fetch(json).then(rep => rep.json()).then((data) => {
         addToPage(data);
     });
 };
 
+// Add to page takes the parsed in data and cycles through ever movie item
+// from the json parse. For each movie, two new list items are created. The
+// proper movie minute and hour format is calculated using divide and mod operations.
+// The text content of both list items are set to the specification appropriate format.
+// Then each list element is given a hidden attribute, minutes, which stores the duration
+// of the movie for later use. A conditional statement adds classList toggles to both items
+// so that each item will have two states when clicked. Finally the first list item is added
+// to the movie database and both list items are passed to addMovie.
+"use strict";
 function addToPage(data) {
-//    ul.addEventListener("click", (e) => {
-//        let li = ol.getElementsByTagName("li");
-//        let valid = false;
-//        for (let i = 0; i < li.length; i++) {
-//            if (li[i].textContent == e.target.textContent) {
-//                valid = true;
-//            }
-//        }
-//        if (!valid) {
-//            ol.appendChild(e.target.cloneNode(true));
-//        } else {
-//            e.target.remove();
-//        }
-//    });
     data.forEach((movie) => {
         const li = document.createElement("li");
         const li2 = document.createElement("li");
         const mins = (parseInt(movie.duration) % 60);
         const hours = ((parseInt(movie.duration) - (parseInt(movie.duration) % 60)) / 60);
-        li.textContent = movie.movie_title + "(" + movie.director_name + ", " + movie.title_year + ", " + hours + ":" + mins + ")";
+        const movieYear = movie.title_year.substring(0, movie.title_year.indexOf("."));
+        li.textContent = movie.movie_title + "(" + movie.director_name + ", " + movieYear + ", " + hours + ":" + mins + ")";
         li2.textContent = li.textContent;
-        if (li.status) {
+        li.setAttribute("minutes", movie.duration);
+        li2.setAttribute("minutes", movie.duration);
+        if (li.status && li2.status) {
             li.classList.add("active");
+            li2.classList.add("active");
         } else {
             li.classList.add("inactive");
         }
@@ -54,6 +55,18 @@ function addToPage(data) {
     });
 };
 
+// Add movie takes both list items and adds event listeners to them.
+// The first list item is the one in the database and it's event listener
+// checks to see if it has been clicked. When it has, it toggles its classList,
+// active which reformats the color and size of clicked items. A conditional
+// statement determines the status of the element, and adds or removes the second
+// list item to the queue list. The second is just a clone of the first to be added
+// to the new list. Update info is then given this item.
+// The second item also has an event listener which toggles the status of list
+// item one when it is clicked. This prevents miscommunication between the two
+// lists and enables a smooth transition when clicked. Finally the item is removed
+// from the queue and updateInfo is given this item.
+"use strict";
 function addMovie(li, li2) {
     li.addEventListener("click", (e) => {
         li.classList.toggle("active");
@@ -64,20 +77,53 @@ function addMovie(li, li2) {
         } else {
             ol.append(li2);
         }
-//    li2.addEventListener("click", (e) => {
-//        li.classList.toggle("inactive");
-//        li.classList.toggle("active");
-//        ol.removeChild(li2);
-//        });
+        updateInfo(li);
+    });
+    li2.addEventListener("click", (e) => {
+        li.classList.toggle("inactive");
+        li.classList.toggle("active");
+        ol.removeChild(li2);
+        updateInfo(li2);
     });
 }
 
+// Update Info takes a given "movie", via a list item and updates the queue
+// info section. First the length of the queue is found with a quick .length call
+// to the ordered list. Then a conditional statement checks to see if the provided
+// movie is set to active. If it is then that means the movie is in the queue and
+// its duration needs to be added to the total. If not then the movie's duration
+// is removed from the total. Previously, addToPage() gave each of these elements
+// a minutes hidden data. This is used for calculating duration.
+// Divide and mod operators calculate the HH:MM format. Finally this info is updated
+// in the "counter" paragraph.
+"use strict";
+function updateInfo(li) {
+    const length = ol.getElementsByTagName("li").length;
+    const time = parseInt(li.getAttribute("minutes"));
+    const active = li.classList.contains("active");
+    if (active) {
+        totalTime += time;
+    } else {
+        totalTime -= time;
+    }
+    const mins = (parseInt(totalTime) % 60);
+    const hours = ((parseInt(totalTime) - (parseInt(totalTime) % 60)) / 60);
+    document.getElementById("counter").innerHTML = "# of Movies: " + length + "<br>Watch Time: " + hours + ":" + mins;
+}
+
+// Search contains the functionality for the search bar. Some variables are defined
+// for necessary information. The input of the actual search bar is grabbed. It is then
+// converted to lower case for ease of use. A for loop cycles through every element in
+// the movie database list. The textContent of each is grabbed. If both the search input and item
+// are equal to each other, the list item is scrolled into view and a yellow highlight is
+// added. Otherwise the format of each element is set back to normal.
+"use strict";
 function search() {
     let input = document.getElementById("searchbar");
     let search = input.value.toLowerCase();
     let li = ul.getElementsByTagName("li");
     for (let i = 0; i < li.length; i++) {
-        let text = li[i].textContent.toLowerCase().substring(0, li[i].textContent.indexOf("(") - 1);
+        let text = li[i].textContent.toLowerCase();
         if (search && text.includes(search)) {
             li[i].scrollIntoView();
             li[i].style.backgroundColor = "yellow";
